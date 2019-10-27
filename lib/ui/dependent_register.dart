80af391f66dacc580c75e50ee7314d8e300cd059
Joao Vitor Rodrigues <joao.rodrigues@luizalabs.com>
2019-10-27 03:06:16 -0300

    Register dependent

---
+++ lib/ui/dependent_register.dart
@@ -0,0 +1,259 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'dart:async';
+import 'package:camera/camera.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
+
+class DependentRegister extends StatefulWidget {
+  Map<String, dynamic> _creche;
+
+  DependentRegister(creche) {
+    this._creche = creche;
+  }
+
+  @override
+  _DependentRegisterState createState() => _DependentRegisterState(_creche);
+}
+
+class _DependentRegisterState extends State<DependentRegister> {
+  Map<String, dynamic> _creche;
+  String path;
+  List<CameraDescription> cameras;
+  CameraController controller;
+  bool isReady = false;
+  final _nomeController = TextEditingController();
+  final _dataNascController = MaskedTextController(mask: '00/00/0000');
+  final _cpfController = MaskedTextController(mask: '000.000.000-00');
+
+  _DependentRegisterState(creche) {
+    this._creche = creche;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupCameras();
+  }
+
+  Future<void> setupCameras() async {
+    try {
+      cameras = await availableCameras();
+      controller = new CameraController(cameras[0], ResolutionPreset.max);
+      await controller.initialize();
+    } on CameraException catch (_) {
+      setState(() {
+        isReady = false;
+      });
+    }
+    setState(() {
+      isReady = true;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro de dependente'),
+      ),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: TextFormField(
+                  cursorColor: Colors.blue,
+                  controller: _nomeController,
+                  decoration: InputDecoration(
+                    labelText: 'Nome completo',
+                    labelStyle: TextStyle(color: Colors.blue),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1
+                      )
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1
+                      )
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 1
+                      )
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 1
+                      )
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Insira o nome!';
+                    }
+                    return null;
+                  },
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: TextFormField(
+                  cursorColor: Colors.blue,
+                  controller: _dataNascController,
+                  decoration: InputDecoration(
+                    labelText: 'Data de nascimento',
+                    labelStyle: TextStyle(color: Colors.blue),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1
+                      )
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1
+                      )
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 1
+                      )
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 1
+                      )
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Insira a data de nascimento!';
+                    }
+                    return null;
+                  },
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: TextFormField(
+                  cursorColor: Colors.blue,
+                  controller: _cpfController,
+                  decoration: InputDecoration(
+                    labelText: 'CPF',
+                    labelStyle: TextStyle(color: Colors.blue),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1
+                      )
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.blue,
+                        width: 1
+                      )
+                    ),
+                    errorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 1
+                      )
+                    ),
+                    focusedErrorBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                        width: 1
+                      )
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Insira o CPF!';
+                    }
+                    return null;
+                  },
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            
+              RaisedButton(
+                child: Icon(Icons.camera_alt),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Container(
+                        child: Stack(
+                          alignment: Alignment.bottomCenter,
+                          children: <Widget>[
+                            CameraPreview(controller), 
+                            RaisedButton(
+                              child: Icon(Icons.camera),
+                              onPressed: () async {
+                                path = join(
+                                  // Store the picture in the temp directory.
+                                  // Find the temp directory using the `path_provider` plugin.
+                                  (await getTemporaryDirectory()).path,
+                                  '${DateTime.now()}.png',
+                                );
+                                controller.takePicture(path).then((_) {
+                                  Navigator.pop(context);
+                                });
+                              },
+                            )
+                          ],
+                        ),
+                      )
+                    )
+                  );
+                },
+              ),
+            
+              path != null ? Container(child: Image.file(File(path),), height: 300) : Container(height: 300,)
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.green,
+        child: Icon(Icons.check),
+        onPressed: () async {
+          var nome = _nomeController.value.text;
+          var cpf = _cpfController.value.text;
+          var dataNasc = _dataNascController.value.text;
+
+          var response = await http.post(
+            'https://hackathon-facef-api.herokuapp.com/dependentes',
+            body: {
+              'nome': nome,
+              'cpf': cpf,
+              'dataNascimento': dataNasc,
+              'creche': _creche
+            }
+          );
+
+          return json.decode(response.body);
+        },
+      ),
+    );
+  }
+}
---
+++ lib/ui/lista_creche.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+import 'package:hackathon_facef_app/ui/dependent_register.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+
+class ListaCreche extends StatefulWidget {
+  @override
+  _ListaCrecheState createState() => _ListaCrecheState();
+}
+
+class _ListaCrecheState extends State<ListaCreche> {
+   List _creches = [];
+
+  Future<Null> _refresh() async {
+    _getCreches().then((data) {
+      setState(() {
+        _creches = data;
+      });
+    });
+  }
+
+  Future<List> _getCreches() async {
+    try {
+      final data = await http.get('https://filadacreche.sme.prefeitura.sp.gov.br/api/v1/schools/radius/wait/-46.643394/-23.615177/4');
+
+      if (data.statusCode == 200) {
+
+        final response = json.decode(data.body);
+        return response['results']['schools'];
+      } else {
+        print('deu merda');
+      }
+
+    } catch (e) {
+      return null;
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getCreches().then((data) {
+      setState(() {
+        _creches = data;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cadastro de dependente'),
+      ),
+      body: ListView.builder(
+        itemCount: _creches.length,
+        itemBuilder: (context, index) {
+          var distance = _creches[index]['distance'] * 1000;
+          return ListTile(
+            title: Text(_creches[index]['nm_exibicao_unidade_educacao']),
+            subtitle: Text(distance.round().toString() + ' metros de distância'),
+            onTap: () {
+              Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DependentRegister(_creches[index])
+                    )
+                  );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
