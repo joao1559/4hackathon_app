25a760fc87dd2ed84c5ac8657f04765d0db11445
Joao Vitor Rodrigues <joao.rodrigues@luizalabs.com>
2019-10-27 00:04:14 -0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hackathon_facef_app/ui/lista_creche.dart';
 import 'package:hackathon_facef_app/ui/login.dart';
 
 void main() {
   runApp(MaterialApp(
       title: 'Hackathon Facef',
-      home: Login(),
+      home: ListaCreche(),
       theme: ThemeData(
           primaryColor: Colors.blue, accentColor: Colors.amberAccent)));
 }
