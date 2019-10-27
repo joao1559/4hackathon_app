26a7cb101b41625e14a35bdd32cdbe9922a1bd6e
Guilherme H <guilherme.assis@luizalabs.com>
2019-10-26 22:04:55 -0300

    Implementando perfil responsavel

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:hackathon_facef_app/ui/login.dart';
+// import 'package:hackathon_facef_app/ui/login.dart';
+import 'package:hackathon_facef_app/ui/responsavel_profile.dart';
 
 void main() {
   runApp(MaterialApp(
       title: 'Hackathon Facef',
-      home: Login(),
+      home: ResponsavelProfile('1'),
       theme: ThemeData(
-        primaryColor: Colors.blue, accentColor: Colors.amberAccent
-      )
-    ));
+          primaryColor: Colors.blue, accentColor: Colors.amberAccent)));
 }
