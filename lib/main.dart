30727b41153272b8c5edb835ecc999eea20c6a4e
Guilherme H <guilherme.assis@luizalabs.com>
2019-10-26 22:13:40 -0300

    Voltando Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:hackathon_facef_app/ui/login.dart';
-import 'package:hackathon_facef_app/ui/responsavel_profile.dart';
+import 'package:hackathon_facef_app/ui/login.dart';
 
 void main() {
   runApp(MaterialApp(
       title: 'Hackathon Facef',
-      home: ResponsavelProfile('1'),
+      home: Login(),
       theme: ThemeData(
           primaryColor: Colors.blue, accentColor: Colors.amberAccent)));
 }
